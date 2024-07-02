30f3cd35ac2771df7968579a9244a0f71a431674
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-07-02 23:11:58 +0530

    changed

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export const axiosi=axios.create({withCredentials:false,baseURL:process.env.REACT_APP_BASE_URL})
+export const axiosi=axios.create({withCredentials:false,baseURL:process.env.REACT_APP_BASE_URL,headers:process.env.REACT_APP_HEADERS})
