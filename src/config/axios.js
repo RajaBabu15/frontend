f6204ee0edf45248396d65e4e0906ced7393a1d1
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-07-02 07:14:39 +0530

    builder is building

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,6 @@
 import axios from 'axios'
 
-export const axiosi=axios.create({baseURL:process.env.REACT_APP_BASE_URL})
+export const axiosi=axios.create({withCredentials: false,baseURL:process.env.REACT_APP_BASE_URL,headers: {
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    }})
