518fefb78e5aaabae72f8abd1f0abe42e6cf07b6
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-07-02 23:37:07 +0530

    fgsdf

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
 
-export const axiosi = axios.create({ withCredentials: false, baseURL: process.env.REACT_APP_BASE_URL, headers: { 'Access-Control-Allow-Origin': 'https://frontend-iota-beryl-58.vercel.app' } })
+export const axiosi = axios.create({ withCredentials: false, baseURL: process.env.REACT_APP_BASE_URL })
